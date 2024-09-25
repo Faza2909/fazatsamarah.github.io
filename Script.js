c0f7cc54797931ff6e2e2f4ff1eeebbd704c0859
fazatsamarah <fzatsamarah@gmail.com>
2024-09-25 07:29:39 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+// Smooth scroll for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+// Toggle hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
